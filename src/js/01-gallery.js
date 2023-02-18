3e3eaf29223775256dd894b4095dde4b9266799e
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-02-18 17:24:06 +0200

    add_lightBox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox"
+
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+const gallery = document.querySelector(`.gallery`);
+
+const imageList = 
+    galleryItems.map(
+        ({preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+          <img 
+          class="gallery__image"
+          src="${preview}" 
+          data-sourse="${original}"
+          alt="${description}">
+          </a>`
+    }).join(``);
+
+    gallery.insertAdjacentHTML('afterbegin', imageList);
+
+    var lightbox = new SimpleLightbox('.gallery a', {  captionDelay: 250,
+          captionsData: "alt",});
+
