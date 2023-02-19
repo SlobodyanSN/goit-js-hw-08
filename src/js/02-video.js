3d199757e6ef94f900329c6cf2ce10475fd864ac
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-02-19 20:30:41 +0200

    2-d_task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector(`#vimeo-player`)
+
+const player = new Player(iframe,);
+
+const currentTime = throttle(function(data) {
+   
+    localStorage.setItem("videoplayer-current-time",
+        JSON.stringify(data.seconds));
+},1000 );
+
+
+player.on('timeupdate', currentTime);
+
+
+
+
+
+
+let startTime = Number(localStorage.getItem("videoplayer-current-time"))
+
+
+player.setCurrentTime(startTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
