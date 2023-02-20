718cd703bdf14a690821ffcb99453c2b2d47134a
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-02-20 21:23:53 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(`.feedback-form`);
+const textarea = document.querySelector(`textarea`)
+const email = document.querySelector(`input`)
+getDataLocalStorage()
+
+function onDataForm (event) {
+        const formElements = event.currentTarget.elements;
+      const email = formElements.email.value;
+      const message = formElements.message.value;
+      const formData = {
+        email,
+        message,
+      }; 
+
+localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+};
+
+form.addEventListener(`input`, onDataForm);
+form.addEventListener(`submit`, onSubmitForm);
+
+function onSubmitForm(event){
+    event.preventDefault();
+    if (email.value === "" || textarea.value.trim() === "") {
+        alert("Додайте будь ласка відгук")
+        return
+      }
+
+    event.currentTarget.reset();
+
+    localStorage.removeItem("feedback-form-state")
+};
+
+function getDataLocalStorage(){
+    const savedString = localStorage.getItem("feedback-form-state");
+    if (savedString) {
+        const savedData = JSON.parse(savedString)
+
+        textarea.value = savedData.message;
+
+        email.value = savedData.email
+
+        console.log(savedData);
+    }
+};
